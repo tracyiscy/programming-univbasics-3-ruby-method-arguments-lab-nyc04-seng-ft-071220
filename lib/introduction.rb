3a1d681a83fb6d20c8124581bb01db23f8d1fbbd
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-15 17:18:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,10 +5,16 @@
 
 
 def introduction(name)
-  "hi, my name is #{name}."
+  puts "hi, my name is #{name}."
 end
 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+def introduction_with_language_optional(name,language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
